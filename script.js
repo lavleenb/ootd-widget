c1747ce23c865aea32f0f91cea1ed84ad18e640d
lavleen <lavleenbhachu4@gmail.com>
2026-05-23 01:44:20 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,30 @@
+function showModal(modalId) {
+    const modal = document.getElementById(modalId);
+    if (modal) {
+        modal.classList.add('show');
+    }
+}
+
+function closeModal(modal) {
+    if (modal) {
+        modal.classList.remove('show');
+    }
+}
+
+// Add event listeners to images with data-modal attribute
+const images = document.querySelectorAll('button[modal-id]');
+images.forEach(img => {
+    img.addEventListener('click', () => {
+        const modalId = img.getAttribute('modal-id');
+        showModal(modalId);
+    });
+});
+
+// Add event listeners to all close buttons
+const closeButtons = document.querySelectorAll('.modal-close-btn');
+closeButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const modal = button.closest('.modal');
+        closeModal(modal);
+    });
+});
